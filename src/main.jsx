78817c7afaa338668ee2d9dc8557f126d0ae7436
Edwin Zambrano <eaespinosa9312@hotmail.com>
2023-03-28 16:07:27 -0500

    Practica ReactRouter Final

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import ErrorPage from './error-page'
 import './index.css'
 import Contact, {
   loader as contactLoader,
+  action as contactAction,
 } from './routes/contact';
 import EditContact, {action as editAction} from './routes/edit'
 import { action as destroyAction } from './routes/destroy'
@@ -18,24 +19,32 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     loader: rootLoader,
     action: rootAction,
+    errorElement: <ErrorPage />,
     children: [
-        { index: true, element: <Index /> },
-        {
-          path: "contacts/:contactId",
-          element: <Contact />,
-          loader: contactLoader,
-        }, 
-        {
-          path: "contacts/:contactId/edit",
-          element: <EditContact />,
-          loader: contactLoader,
-          action: editAction,
-        },
-        {
-          path: "contacts/:contactId/destroy",
-          action: destroyAction,
-          errorElement: <div>Oops! there was an error.</div>
-        },
+      {
+        errorElement: <ErrorPage />,
+        children:[
+          { index: true, element: <Index /> },
+          {
+            path: "contacts/:contactId",
+            element: <Contact />,
+            loader: contactLoader,
+            action: contactAction,
+          }, 
+          {
+            path: "contacts/:contactId/edit",
+            element: <EditContact />,
+            loader: contactLoader,
+            action: editAction,
+          },
+          {
+            path: "contacts/:contactId/destroy",
+            action: destroyAction,
+            errorElement: <div>Oops! there was an error.</div>
+          },
+        ],
+      },
+        
     ],
 
   },
